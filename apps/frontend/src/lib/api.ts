6854ce78a85222e0ce289860bc5a13d385564134
josecancino <jose.cancino@outlook.com>
2026-01-23 22:50:38 +0200

    chore: remove unused REST endpoint

--- apps/frontend/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-export const ArticleService = {
-  async delete(id: string): Promise<boolean> {
-    const res = await fetch(`/api/articles/${id}`, { method: 'DELETE' });
-    return res.ok;
-  },
-};
